afd6cf55ef8369660594d35445fa765edef08346
fernando_dona <fernando.fdona@gmail.com>
2025-10-07 21:15:44 -0300

    refactoring tests for result

--- tests/SimpleResult.Tests/ResultTests.cs
+++ tests/SimpleResult.Tests/ResultTests.cs
@@ -5,13 +5,10 @@ public class ResultTests
     [Fact]
     public void Create_SuccessResult()
     {
-        // Arrange
         var value = "value";
 
-        // Act
         Result result = Result.Success();
 
-        // Assert
         Assert.True(result.IsSuccess);
         Assert.False(result.IsFailure);
     }
@@ -19,16 +16,12 @@ public class ResultTests
     [Fact]
     public void Create_FailureResult()
     {
-        // Arrange
-        var error = new Error(999, "Test.Error", "Test error message", ErrorType.InternalServerError);
+        var error = new Error("Test.Error", "Test error message", ErrorType.InvalidData);
 
-        // Act
         Result result = error;
 
-        // Assert
         Assert.False(result.IsSuccess);
         Assert.True(result.IsFailure);
-        Assert.Equal(error, result.FirstError);
         Assert.Single(result.Errors);
         Assert.Equal(error, result.Errors[0]);
     }
@@ -36,33 +29,29 @@ public class ResultTests
     [Fact]
     public void Create_FailureResult_ListErrors()
     {
-        // Arrange
         var errors = new List<Error>
         {
-            new Error(998, "Test.Error1", "Test error message", ErrorType.NotImplemented),
-            new Error(999, "Test.Error2", "Test error message", ErrorType.InternalServerError)
+            new Error("Test.Error1", "Test error message", ErrorType.Conflict),
+            new Error("Test.Error2", "Test error message", ErrorType.RuleViolation)
         };
 
-        // Act
         Result result = errors;
 
-        // Assert
         Assert.False(result.IsSuccess);
         Assert.True(result.IsFailure);
-        Assert.Equal(errors[0], result.FirstError);
-        Assert.True(CompareErrors(errors, result.Errors));
+        foreach (var error in errors)
+        {
+            Assert.Contains(error, result.Errors);
+        }
     }
 
     [Fact]
     public void Create_FailureResult_NullErrors()
     {
-        // Arrange
         List<Error>? errors = null;
 
-        // Act
         Action act = () => { Result result = errors!; };
 
-        // Assert
         var exception = Assert.Throws<ArgumentNullException>(act);
         Assert.Equal("An error result must have at least one error (Parameter 'errors')", exception.Message);
     }
@@ -70,13 +59,10 @@ public class ResultTests
     [Fact]
     public void Create_FailureResult_EmptyErrors()
     {
-        // Arrange
         List<Error> errors = new List<Error>();
 
-        // Act
         Action act = () => { Result result = errors; };
 
-        // Assert
         var exception = Assert.Throws<ArgumentNullException>(act);
         Assert.Equal("An error result must have at least one error (Parameter 'errors')", exception.Message);
     }
@@ -84,10 +70,8 @@ public class ResultTests
     [Fact]
     public void Match_Success()
     {
-        // Arrange
         Result result = Result.Success();
 
-        // Act
         var response = result.Match(
             onSuccess: () => true,
             onError: _ => false);
@@ -98,34 +82,13 @@ public class ResultTests
     [Fact]
     public void Match_Failure()
     {
-        // Arrange
-        var error = new Error(999, "Test.Error", "Test error message", ErrorType.InternalServerError);
+        var error = new Error("Test.Error", "Test error message", ErrorType.Unauthorized);
         Result result = error;
 
-        // Act
         var response = result.Match(
             onSuccess: () => "test",
-            onError: _ => "error");
+            onError: e => e.First().Message);
 
-        // Assert
-        Assert.Equal("error", response);
-    }
-
-    private bool CompareErrors(List<Error> errors1, List<Error> errors2)
-    {
-        if (errors1.Count != errors2.Count)
-        {
-            return false;
-        }
-
-        for (int i = 0; i < errors1.Count; i++)
-        {
-            if (errors1[i].Id != errors2[i].Id || errors1[i].Code != errors2[i].Code)
-            {
-                return false;
-            }
-        }
-
-        return true;
+        Assert.Equal(error.Message, response);
     }
 }
--- tests/SimpleResult.Tests/ResultValueTests.cs
+++ tests/SimpleResult.Tests/ResultValueTests.cs
@@ -5,13 +5,10 @@ public class ResultValueTests
     [Fact]
     public void Create_SuccessResult()
     {
-        // Arrange
         var value = "value";
 
-        // Act
         Result<string> result = value;
 
-        // Assert
         Assert.True(result.IsSuccess);
         Assert.False(result.IsFailure);
         Assert.Equal(value, result.Value);
@@ -20,30 +17,23 @@ public class ResultValueTests
     [Fact]
     public void Create_SuccessResult_NullValue()
     {
-        // Arrange
         string? value = null;
 
-        // Act
         Action act = () => { Result<string> result = value!; };
 
-        // Assert
         var exception = Assert.Throws<ArgumentNullException>(act);
-        Assert.Equal("A successful result cannot be null (Parameter 'value')", exception.Message);
+        Assert.Equal("A successful Result<T> cannot be null. (Parameter 'value')", exception.Message);
     }
 
     [Fact]
     public void Create_FailureResult()
     {
-        // Arrange
-        var error = new Error(999, "Test.Error", "Test error message", ErrorType.InternalServerError);
+        var error = new Error("Test.Error", "Test error message", ErrorType.Unexpected);
 
-        // Act
         Result<string> result = error;
 
-        // Assert
         Assert.False(result.IsSuccess);
         Assert.True(result.IsFailure);
-        Assert.Equal(error, result.FirstError);
         Assert.Single(result.Errors);
         Assert.Equal(error, result.Errors[0]);
     }
@@ -51,33 +41,29 @@ public class ResultValueTests
     [Fact]
     public void Create_FailureResult_ListErrors()
     {
-        // Arrange
         var errors = new List<Error>
         {
-            new Error(998, "Test.Error1", "Test error message", ErrorType.NotImplemented),
-            new Error(999, "Test.Error2", "Test error message", ErrorType.InternalServerError)
+            new Error("Test.Error1", "Test error message", ErrorType.Conflict),
+            new Error("Test.Error2", "Test error message", ErrorType.RuleViolation)
         };
 
-        // Act
         Result<string> result = errors;
 
-        // Assert
         Assert.False(result.IsSuccess);
         Assert.True(result.IsFailure);
-        Assert.Equal(errors[0], result.FirstError);
-        Assert.True(CompareErrors(errors, result.Errors));
+        foreach (var error in errors)
+        {
+            Assert.Contains(error, result.Errors);
+        }
     }
 
     [Fact]
     public void Create_FailureResult_NullErrors()
     {
-        // Arrange
         List<Error>? errors = null;
 
-        // Act
         Action act = () => { Result<string> result = errors!; };
 
-        // Assert
         var exception = Assert.Throws<ArgumentNullException>(act);
         Assert.Equal("An error result must have at least one error (Parameter 'errors')", exception.Message);
     }
@@ -85,13 +71,10 @@ public class ResultValueTests
     [Fact]
     public void Create_FailureResult_EmptyErrors()
     {
-        // Arrange
         List<Error> errors = new List<Error>();
 
-        // Act
         Action act = () => { Result<string> result = errors; };
 
-        // Assert
         var exception = Assert.Throws<ArgumentNullException>(act);
         Assert.Equal("An error result must have at least one error (Parameter 'errors')", exception.Message);
     }
@@ -99,50 +82,26 @@ public class ResultValueTests
     [Fact]
     public void Match_Success()
     {
-        // Arrange
         var value = "value";
         Result<string> result = value;
 
-        // Act
         var response = result.Match(
             onSuccess: v => v,
             onError: _ => "error");
 
-        // Assert
         Assert.Equal(value, response);
     }
 
     [Fact]
     public void Match_Failure()
     {
-        // Arrange
-        var error = new Error(999, "Test.Error", "Test error message", ErrorType.InternalServerError);
+        var error = new Error("Test.Error", "Test error message", ErrorType.Forbidden);
         Result<string> result = error;
 
-        // Act
         var response = result.Match(
             onSuccess: v => v,
-            onError: _ => "error");
+            onError: e => e.First().Message);
 
-        // Assert
-        Assert.Equal("error", response);
-    }
-
-    private bool CompareErrors(List<Error> errors1, List<Error> errors2)
-    {
-        if (errors1.Count != errors2.Count)
-        {
-            return false;
-        }
-
-        for (int i = 0; i < errors1.Count; i++)
-        {
-            if (errors1[i].Id != errors2[i].Id || errors1[i].Code != errors2[i].Code)
-            {
-                return false;
-            }
-        }
-
-        return true;
+        Assert.Equal(error.Message, response);
     }
 }
